8e93d799c6ec705dfc73a8ddbf02efc70759f334
danielluccio <daniel130877@gmail.com>
2025-10-20 08:27:36 -0300

    Add da app de authentication

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # App de autenticação do sistema
+    'authentication',
 ]
 
 MIDDLEWARE = [
